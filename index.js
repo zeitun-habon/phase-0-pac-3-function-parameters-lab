e04268078f396d34e8c6e1e08f139f404b6b25c8
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-04 22:13:07 +0300

    commit tree 33c8bc49a7484ec70eb697238da3b1824b7d1561

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function introduction(name){
+    return  "Hi, my name is ${name}";
+}
+introduction("Aki");
+function introductionWithLanguage(name,language){
+    return "Hi, my name is ${name} and i am learning to program in ${language}"
+}
+introductionWithLanguage("Aki", "Ember.js")
